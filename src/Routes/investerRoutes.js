3b1f14e2f8814056a8a313a19744a034ff003a39
AsgharALi <fa21bscs0017@maju.edu.pk>
2024-03-12 00:46:52 +0500

    biolerplate

---
+++ src/Routes/investerRoutes.js
@@ -0,0 +1,8 @@
+
+const { signup } = require('../Controller/controller');
+
+const investerRoutes = async (fastify, options) => {
+    fastify.post("/signup", signup);
+};
+
+module.exports = investerRoutes;
